185c7883750f78b17f03812551dc632b5a5ddc0f
Matthew <matthewpublico@gmail.com>
2025-07-21 01:24:46 -0700

    learning python3

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+"""Module providing a function printing python version."""
+
+# print("Hello World")
+
+# STUDENTS_COUNT = 1000
+# RATING = 2.99
+# IS_PUBLISHED = True
+# COURSE_NAME = "Python Course"
+
+# print(len(COURSE_NAME))
+
+print("Please type a number: ")
+
+x = int(input('>'))
+
+print(x + 15)
