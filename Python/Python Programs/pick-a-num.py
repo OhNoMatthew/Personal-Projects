9e5473eed691354299453d56aab8a73e14170678
Matthew <matthewpublico@gmail.com>
2025-07-25 17:45:45 -0700

    I made a pick a number game. I plan to add new things into it but for now it works fine :D

---
+++ Python/Python Programs/pick-a-num.py
@@ -0,0 +1,126 @@
+import random
+
+bot_name: str = "Jon"
+print(f'Welcome to pick a number! I am {bot_name}, please type a difficulty')
+
+def easy() -> None:
+    while True:
+        # Pick number from within the range
+        random_number: int = random.randint(1,10)
+        print("\nI have my number...")
+        user_guess = int (input("Enter your guess: "))
+        
+        # Check if the nubmer is the same as the bot.
+        if user_guess == random_number:
+            print("You guessed correct!")
+            break
+
+        # Check if the input is a valid number within the range.
+        try:
+            if user_guess >= 10:
+                raise ValueError("The number is not within the range idiot kys")
+        except ValueError:
+            print("Bruh that's not a valid guess... KYS NOW")
+
+        # Not the same number
+        else:
+            print(f'Nice Try! The number was {random_number}')
+            break
+
+def medium() -> None:
+    while True:
+        # Pick number from within the range
+        random_number: int = random.randint(1,30)
+        print("\nI have my number...")
+        user_guess = int (input("Enter your guess: "))
+        
+        # Check if the nubmer is the same as the bot.
+        if user_guess == random_number:
+            print("You guessed correct!")
+            break
+
+        # Check if the input is a valid number within the range.
+        try:
+            if user_guess >= 30:
+                raise ValueError("The number is not within the range idiot kys")
+        except ValueError:
+            print("Bruh that's not a valid guess... KYS NOW")
+
+        # Not the same number
+        else:
+            print(f'Nice Try! The number was {random_number}')
+            break
+
+def hard() -> None:
+    while True:
+        # Pick number from within the range
+        random_number: int = random.randint(1,100)
+        print("\nI have my number...")
+        user_guess: int = (input('Your guess: '))
+        
+        # Check if the nubmer is the same as the bot.
+        if user_guess == random_number:
+            print("You guessed correct!")
+            break
+
+        # Check if the input is a valid number within the range.
+        try:
+            if user_guess >= 100:
+                raise ValueError("The number is not within the range idiot kys")
+        except ValueError:
+            print("Bruh that's not a valid guess... KYS NOW")
+
+        # Not the same number
+        else:
+            print(f'Nice Try! The number was {random_number}')
+            break
+        
+def impossible() -> None:
+    while True:
+        # Pick number from within the range
+        random_number: int = random.randint(1,10000000)
+        print("\nI have my number...")
+        user_guess: int = (input('Your guess: '))
+        
+        # Check if the nubmer is the same as the bot.
+        if user_guess == random_number:
+            print("You guessed correct!")
+            break
+
+        # Check if the input is a valid number within the range.
+        try:
+            if user_guess >= 10000000:
+                raise ValueError("The number is not within the range idiot kys")
+        except ValueError:
+            print("Bruh that's not a valid guess... KYS NOW")
+
+        # Not the same number
+        else:
+            print(f'DAMN you suck! The number was {random_number}')
+            break
+
+def main() -> None:
+    while True:
+        user_input: str = input('*Easy* *Medium* *Hard* > '.lower())
+        wins: int = 0
+
+        # Choose Difficulty
+        if user_input in ['easy']:
+            print(f'You have chosen {user_input}')
+            easy()
+        elif user_input in ['medium']:
+            print(f'You have chosen {user_input}')
+            medium()
+        elif user_input in ['hard']:
+            print(f'You have chosen {user_input}')
+            hard()
+        elif user_input in ['impossible']:
+            print(f'How did you find this mode? Well you asked for it... {user_input} mode.')
+            impossible()
+        elif user_input in ['quit']:
+            print('Program quit')
+            break
+        else:
+            print('Please choose a valid difficulty')
+
+main()
---
+++ Python/program-testing.py
@@ -0,0 +1,20 @@
+import random
+
+def easy() -> None:
+    # Pick number from within the range
+    random_number = random.randint(1,10)
+    print("I have my number...")
+
+    # Check if the input is a valid number within the range.
+    try:
+        user_guess: int = int(input('Your guess: '))
+    except not random.randint(1,10):
+        print('Bruh that\'s not a valid guess...')
+    
+    # Check if the nubmer is the same as the bot.
+    if user_guess == random_number:
+        print("You guessed correct!")
+
+    # Not the same number
+    else:
+        print(f'Nice Try! The number was {random_number}')
