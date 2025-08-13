3f198d242efe7f5f0f7d6472a701f1f3224fc3bf
Matthew <matthewpublico@gmail.com>
2025-08-12 20:11:09 -0700

    Adding another new program!

--- Programs/Python_Programs/to-do-list.py
+++ Programs/Python_Programs/to-do-list.py
@@ -1,48 +1,56 @@
 """ Simple To Do List """
 
-# Create a node for data
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Main:
-
-# Create a list
-    print('Welcome to your To-Do List!')
-    my_task: list = []
-
-# Start the queue list
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.length = 0
+my_task = []
 
 # Add a task
-    def addTask(self, task):
-        new_node = Node(task)
-        if self.rear is None:
-            self.front = self.rear = new_node
-            self.length += 1
-            return
-        self.rear.next = new_node
-        self.rear = new_node
-        self += 1
+def addTask():
+    """ Adding a Task """
+    task = input('Please enter a task: ')
+    my_task.append(task)
+    print(f'Task, {task} has been added to the list!')
+
+# List of tasks
+def checkTask() -> None:
+    """ Check the List of Tasks """
+    if not my_task:
+        print("There are no tasks currently.")
+    else:
+        print('Current Tasks:')
+        for index, task in enumerate(my_task):
+            print(f'Task #{index}. {task}')
 
 # Delete a Task
-    def deleteTask(self):
-        if self.isEmpty():
-            return "Queue is empty"
-        temp = self.front
-        self.front = temp.next
-        self.length -= 1
-        if self.front is None:
-            self.rear = None
-        return temp.data
-    
-# Check if the Task list is empty
-    def isEmpty(self):
-        print('Seems that the list is empty')
-        return self.length == 0
-    
-# Mark a task as done
+def deleteTask():
+    """ Delete a Task """
+    checkTask()
+    try:
+        taskToDelete: int = int(input('Enter the # to delete: '))
+        if taskToDelete >=0 and taskToDelete < len(my_task):
+            my_task.pop(taskToDelete)
+            print(f'Task {taskToDelete} has been removed.')
+        else:
+            print(f'Task #{taskToDelete} was not found.')
+    except TypeError:
+        print('Invalid input.')
+
+# Create a list
+print('Welcome to your To-Do List!')
+
+while True:
+    user_input: str = input('\nType what would you like to do?\n Add, Delete, Check, Quit > ').lower()
+
+    # Check what the user wants to do.
+    if user_input in ['add']:
+        print('Adding a task!\n')
+        addTask()
+    elif user_input in ['delete']:
+        print('Deleting a task!\n')
+        deleteTask()
+    elif user_input in ['check']:
+        print(f'Here is the list!')
+        checkTask()
+    elif user_input in ['quit']:
+        print('Program quit')
+        break
+    else:
+        print('Invalid input.')
